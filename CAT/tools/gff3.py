829272db841b5282a4c851a3673c0715241deb9c
Ian Fiddes <ian.t.fiddes@gmail.com>
2016-09-17 11:47:22 -0700

    Gff3 no longer stores num introns. That will be done by sqlInterface.

--- CAT/tools/gff3.py
+++ CAT/tools/gff3.py
@@ -357,13 +357,10 @@ def extract_attrs(gff3):
                     except KeyError:  # attempt Gencode naming scheme
                         tx_biotype = tx.attributes['transcript_type'][0]
                     feature_types = {c.type for c in tx.children}
-                    num_exons = len([c for c in tx.children if c.type == 'exon'])
                     r = {'TranscriptBiotype': tx_biotype, 'GeneId': gene_id,
                          'GeneName': gene_name, 'GeneBiotype': gene_biotype,
                          'StartCodon': 'start_codon' in feature_types,
-                         'StopCodon': 'stop_codon' in feature_types,
-                         'NumReferenceIntrons': num_exons - 1,
-                         'NumReferenceExons': num_exons}
+                         'StopCodon': 'stop_codon' in feature_types}
                     results[tx_id] = r
                 except KeyError, e:
                     raise GFF3Exception('Unable to parse field {} from the input gff3 on line'.format(e, tx.lineNumber))
@@ -372,6 +369,5 @@ def extract_attrs(gff3):
     df = pd.DataFrame.from_dict(results, orient='index')
     df.index.rename('TranscriptId', inplace=True)
     # make into a nice order for the sqlite database
-    df = df[['TranscriptBiotype', 'GeneId', 'GeneName', 'GeneBiotype', 'StartCodon', 'StopCodon',
-             'NumReferenceIntrons', 'NumReferenceExons']]
+    df = df[['TranscriptBiotype', 'GeneId', 'GeneName', 'GeneBiotype', 'StartCodon', 'StopCodon']]
     return df
