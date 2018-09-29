7b3aa93941d4e8e9283972784dc9d3c12dc9ad7a
Chang Kim <dfrontier1@gmail.com>
2018-09-29 13:48:17 -0700

    Fixed gff3 parser to get the latest parser version in tools
    Test fails with current parser version... Also had to edit in browser due to not being able to commit latest changes (git issues).

--- test_parser/parser.py
+++ test_parser/parser.py
@@ -1,19 +1,20 @@
 # test for gff parser
 from pandas.util.testing import assert_frame_equal
 import pandas as pd
+import sys
+import tools.gff3
 
 def test_ncbi():
-   test = run("ncbiGp","ncbiAttrs")
-   ncbi = pd.read_csv("ncbiTest",sep='\t',index_col=0)
+   test = tools.gff3.parse_gff3("test_parser/ncbiGp","test_parser/ncbiAttrs")
+   ncbi = pd.read_csv("test_parser/ncbiTest",sep='\t',index_col=0)
    assert_frame_equal(test,ncbi,check_dtype=False)
 
 def test_gencode():
-   test = run("gencodeGp","gencodeAttrs")
-   gencode = pd.read_csv("gencodeTest",sep='\t',index_col=0)
+   test = tools.gff3.parse_gff3("test_parser/gencodeGp","test_parser/gencodeAttrs")
+   gencode = pd.read_csv("test_parser/gencodeTest",sep='\t',index_col=0)
    assert_frame_equal(test,gencode,check_dtype=False)
 
 def test_ensembl():
-   test = run("ensemblGp","ensemblAttrs")
-   ensembl = pd.read_csv("ensemblTest",sep='\t',index_col=0)
+   test = tools.gff3.parse_gff3("test_parser/ensemblGp","test_parser/ensemblAttrs")
+   ensembl = pd.read_csv("test_parser/ensemblTest",sep='\t',index_col=0)
    assert_frame_equal(test,ensembl,check_dtype=False)
-
