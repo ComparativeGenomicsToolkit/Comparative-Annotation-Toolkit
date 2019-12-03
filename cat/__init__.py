42dace5267776cef3ea2a5225d6d37fd779f7d19
Ian Fiddes <ian.fiddes@inscripta.com>
2019-12-03 12:42:54 -0800

    Revert "more bugs"
    This reverts commit 8c120795fb9fc7a7023674277b33aa4b7ea97aeb.

--- cat/__init__.py
+++ cat/__init__.py
@@ -1047,9 +1047,9 @@ class BuildDb(PipelineTask):
         for genome in list(pipeline_args.target_genomes) + [pipeline_args.ref_genome]:
             args = BuildDb.get_args(pipeline_args, genome)
             logger.info('Loading sequence for {} into database.'.format(genome))
-            cmd = ['load2sqlitedb', '--noIdx', '--clean', '--species={}'.format(genome),
-                   '--dbaccess={}'.format(pipeline_args.hints_db), args.fasta]
-            tools.procOps.run_proc(cmd, stdout='/dev/null', stderr='/dev/null')
+            base_cmd = ['load2sqlitedb', '--noIdx', '--clean', '--species={}'.format(genome),
+                        '--dbaccess={}'.format(pipeline_args.hints_db)]
+            tools.procOps.run_proc(base_cmd + [args.fasta], stdout='/dev/null', stderr='/dev/null')
             if os.path.getsize(args.hints_path) != 0:
                 logger.info('Loading hints for {} into database.'.format(genome))
                 tools.procOps.run_proc(base_cmd + [args.hints_path], stderr='/dev/null')
