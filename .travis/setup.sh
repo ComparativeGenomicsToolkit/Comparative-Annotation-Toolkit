89542641a435f0472b1eeab64fdfbfb54c5689b7
Ian Fiddes <ian.fiddes@inscripta.com>
2020-04-02 09:55:21 -0700

    another attempt

---
+++ .travis/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
+
+# Check Python
+
+echo "Python Version:"
+python --version
+pip install --user sregistry[all]
+sregistry version
+
+echo "sregistry Version:"
+
+# Install Singularity
+
+SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
+export PATH="${GOPATH}/bin:${PATH}"
+
+mkdir -p "${GOPATH}/src/github.com/sylabs"
+cd "${GOPATH}/src/github.com/sylabs"
+
+git clone -b release-3.2 https://github.com/sylabs/singularity
+cd singularity
+./mconfig -v -p /usr/local
+make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
+sudo make -C ./builddir install
