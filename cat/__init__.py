aa6e6b3e8b32e8ffa8a641e54c5fbcfcb409133b
Ian Fiddes <ian.fiddes@inscripta.com>
2020-09-11 13:01:48 -0700

    .

--- cat/__init__.py
+++ cat/__init__.py
@@ -961,7 +961,7 @@ class TranscriptFasta(AbstractAtomicFileTask):
 
     def run(self):
         logger.info('Extracting reference annotation fasta.')
-        seq_dict = tools.bio.get_sequence_dict(self.fasta, upper=False)
+        seq_dict = tools.bio.get_sequence_dict(self.transcript_fasta, upper=False)
         seqs = {tx.name: tx.get_mrna(seq_dict) for tx in tools.transcripts.transcript_iterator(self.transcript_bed)}
         with self.output().open('w') as outf:
             for name, seq in seqs.items():
